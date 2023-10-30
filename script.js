a256caedacb922dcfcab15c1c9a074c507e27541
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-10-30 19:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 //complete this code
-class Animal {}
+class Animal {
+	 constructor(name) {
+        this.name = name;
+    }
 
-class Dog extends Animal {}
+    makeSound() {
+        console.log(`${this.name} makes a sound.`);
+    }
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	 constructor(name) {
+        super(name);
+    }
 
+    makeSound() {
+        console.log(`${this.name} barks: Woof! Woof!`);
+    }
+}
+
+class Cat extends Animal {
+	 constructor(name) {
+        super(name);
+    }
+
+    makeSound() {
+        console.log(`${this.name} purrs: Purr... Purr...`);
+    }
+}
+// Usage
+const myDog = new Dog("Buddy");
+const myCat = new Cat("Whiskers");
+
+myDog.makeSound(); // Outputs: Buddy barks: Woof! Woof!
+myCat.makeSound(); // Outputs: Whiskers purrs: Purr... Purr!
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
